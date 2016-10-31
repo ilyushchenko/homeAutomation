be5e5b73c74ef6b73f8e65b8dce3fb71db5c1ce0
Pavel Ilyushchenko <pavel.ks1996@gmail.com>
2016-10-31 18:28:30 +0300

    Init Commit

---
+++ MainRoomController/MainRoomController.ino
@@ -0,0 +1,35 @@
+#include <Button.h>
+#include <RCSwitch.h>
+
+Button button(8, 50);
+RCSwitch mySwitch = RCSwitch();
+
+long intime;
+int outPin = 7;
+int state = LOW;
+
+
+void setup() {
+  mySwitch.enableReceive(0);
+  button.begin(INPUT);
+  pinMode(outPin, OUTPUT);
+  digitalWrite(outPin, state);
+}
+
+void loop() {
+  if (button.pressed()) {
+    Switch();
+  }
+  if (mySwitch.available())
+  {
+    int value = mySwitch.getReceivedValue();
+    mySwitch.resetAvailable();
+    if(value == 20928) Switch();
+  }
+}
+
+void Switch()
+{
+  state = (state == HIGH) ? LOW : HIGH;
+  digitalWrite(outPin, state);
+}
